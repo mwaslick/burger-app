cd0dd4b3324d8e00260e2bf82c21b891daad1bb9
Maria Waslick <mariawaslick@gmail.com>
2020-10-15 21:39:25 -0400

    got add and eat functions working

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,16 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
